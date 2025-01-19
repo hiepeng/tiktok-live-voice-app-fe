907a361516a7782bdf7d7be4ffac810b514603a1
Default <hiepnvna@gmail.com>
2025-01-19 18:57:47 +0700

    save code

--- constants/config.ts
+++ constants/config.ts
@@ -1,4 +1,4 @@
 const DEV_BACKEND_URL = 'http://192.168.1.109:4000'; // Thay bằng IP máy tính của bạn
-const PROD_BACKEND_URL = 'https://your-production-url.com';
+const PROD_BACKEND_URL = 'https://api.downloadcomment.com';
 
 export const BACKEND_URL = (__DEV__ ? DEV_BACKEND_URL : PROD_BACKEND_URL) + '/api/v1';
